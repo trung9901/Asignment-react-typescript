761baf4216e6010936e9c6730acf4ae229b38cc0
Trung <trungnhph13487@fpt.edu.vn>
2022-04-04 19:43:26 +0700

    a

--- src/pages/layouts/AdminLayout.tsx
+++ src/pages/layouts/AdminLayout.tsx
@@ -22,7 +22,7 @@ const AdminLayout = (props: Props) => {
             <div className="container-fluid">
                 <div className="row">
                     <Sidebar />
-                    <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4"><div className="chartjs-size-monitor"><div className="chartjs-size-monitor-expand"><div /></div><div className="chartjs-size-monitor-shrink"><div /></div></div>
+                    <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
                         <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                             <h1 className="h2">Dashboard</h1>
 
